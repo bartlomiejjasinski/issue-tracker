ce62721789ac24e4d4df967bf6050b0ca103e2be
FISH1 <fish1@FISH1s-Mac-mini.local>
2020-01-26 17:52:57 +0100

    configure database connection

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,4 +1,5 @@
 import * as express from 'express';
+import * as mongoose from 'mongoose';
 import { Application } from 'express';
 import { config } from './config/config.env';
 
@@ -9,12 +10,23 @@ class Server {
 
     constructor() {
         this._app = express();
+        this.configMongoose();
     }
 
     public start(): void {
 
         this._app.listen(port, () => {
             console.log(`Start server on port: ${port}`);
+        });
+    }
+
+    private configMongoose() {
+        mongoose.connect(config.database.url, err => {
+            if (err) {
+                console.error(err);
+            } else {
+                console.log(`connected to: ${config.database.url}`);
+            }
         });
     }
 }
